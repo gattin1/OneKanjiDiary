9a32bd51935ed4568d197f96379e8bb906f4dfcd
Kouga <baseballkouga1998@gmail.com>
2024-07-30 19:20:53 +0900

    日記編集、削除機能

--- app/controllers/diaries_controller.rb
+++ app/controllers/diaries_controller.rb
@@ -1,11 +1,10 @@
 class DiariesController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_diary, only: [:show, :edit, :update, :destroy]
   def index
   end
 
-  def show
-    @diary = current_user.diaries.find(params[:id])
-  end
+  def show;end
 
   def new
     @diary = current_user.diaries.build
@@ -14,22 +13,32 @@ class DiariesController < ApplicationController
   def create
     @diary = current_user.diaries.build(diary_params)
     if @diary.save
-      redirect_to user_diaries_path(current_user), notice: '日記が作成されました。'
+      redirect_to user_diaries_path(current_user, @diary), notice: '日記が作成されました。'
     else
-      render :new
+      render :new, status: :unprocessable_entity
     end
   end
 
-  def edit
-  end
+  def edit;end
 
   def update
+    if @diary.update(diary_params)
+      redirect_to user_diary_path(current_user, @diary), notice: '日記が更新されました。'
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    @diary.destroy
+    redirect_to user_diaries_path(current_user), notice: '日記が削除されました。'
   end
 
   private
+
+  def set_diary
+    @diary = current_user.diaries.find(params[:id])
+  end
 
   def diary_params
     params.require(:diary).permit(:content, :memo, :date)
