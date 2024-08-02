06a0f4f6710fccbbcb171c89569bcf60dc8d3139
Kouga <baseballkouga1998@gmail.com>
2024-08-03 08:15:35 +0900

    日記カレンダー表示

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,10 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def after_sign_in_path_for(resource)
+    user_diaries_path(resource) # ログインしたユーザーの日記一覧ページへリダイレクト
+  end
+
   protected
 
   def configure_permitted_parameters
