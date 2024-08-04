b22df96a002c2d516fad90b0c643d717354a596e
Kouga <baseballkouga1998@gmail.com>
2024-08-05 06:20:00 +0900

    気分によって表示する文字を変える

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,9 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Mood.create(name: '喜', color: 'yellow')
+Mood.create(name: '怒', color: 'red')
+Mood.create(name: '哀', color: 'blue')
+Mood.create(name: '楽', color: 'orange')
+Mood.create(name: '驚', color: 'purple')
+Mood.create(name: '無', color: 'gray')
