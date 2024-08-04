e3c04b751495b63b440a6385e4e1ae294efea538
Kouga <baseballkouga1998@gmail.com>
2024-08-05 06:20:00 +0900

    気分によって表示する文字を変える

--- app/models/diary.rb
+++ app/models/diary.rb
@@ -4,6 +4,7 @@
 # 日記の内容、メモ、日付などの属性を持ちます。
 class Diary < ApplicationRecord
   belongs_to :user
+  belongs_to :mood
 
   validates :title, presence: true, length: { is: 1 }
   validates :memo, length: { maximum: 255 }
