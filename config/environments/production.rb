13d435a2febf262fc578f95f237c05d7da462622
Kouga <baseballkouga1998@gmail.com>
2024-08-13 19:55:36 +0900

    再々

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,13 +23,13 @@ Rails.application.configure do
   # config.require_master_key = true
 
   # Disable serving static files from `public/`, relying on NGINX/Apache to do so instead.
-  config._server.enabled = true
+  config.public_file_server.enabled = true
 
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
 
   # Do not fall back to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  config.assets.compile = false
 
   config.assets.digest = true
 
