3c97ffbcf5962f2ea007003a98397a5bf21e21f0
Kouga <baseballkouga1998@gmail.com>
2024-09-25 03:22:50 +0900

    puma

--- config/puma.rb
+++ config/puma.rb
@@ -22,10 +22,11 @@ end
 
 # Specifies the `worker_timeout` threshold that Puma will use to wait before
 # terminating a worker in development environments.
-worker_timeout 3600 if ENV.fetch('RAILS_ENV', 'development') == 'development'
+worker_timeout 60 if ENV.fetch('RAILS_ENV', 'development') == 'development'
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 port ENV.fetch('PORT', 3000)
+bind "tcp://0.0.0.0:#{ENV.fetch('PORT')}"
 
 # Specifies the `environment` that Puma will run in.
 environment ENV.fetch('RAILS_ENV', 'development')
@@ -35,6 +36,3 @@ pidfile ENV.fetch('PIDFILE', 'tmp/pids/server.pid')
 
 # Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
-
-bind "tcp://0.0.0.0:#{ENV.fetch('PORT', 3000)}"
-
